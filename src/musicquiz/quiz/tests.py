e0b36bcdd3bef51a99fbcbb1d86420c2ceb4c7d9
Marius Damarackas <m.damarackas@gmail.com>
2010-04-28 11:45:35 +0300

    Added homophony and simple functional test

--- src/musicquiz/quiz/tests.py
+++ src/musicquiz/quiz/tests.py
@@ -2,6 +2,7 @@ import unittest
 import doctest
 import forms
 from django.test import TestCase
+import homophony
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -13,6 +14,7 @@ class SimpleTest(TestCase):
 def suite():
     suite = unittest.TestSuite([
         SimpleTest('test_basic_addition'),
-        doctest.DocTestSuite(forms)
+        doctest.DocTestSuite(forms),
+        homophony.DocFileSuite('tests.txt'),
     ])
     return suite
